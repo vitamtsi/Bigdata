f35544add479bb33e5fdaee1d5c94b8ecc738af3
vitamtsi <vita.maurite@gmail.com>
2025-12-16 10:18:36 +0200

    Update project7_app.py

--- project7_app.py
+++ project7_app.py
@@ -342,10 +342,7 @@ with tab5:
 
     forecast_df = pd.DataFrame({"Month": future_months, "Predicted NO2": preds})
 
-    st.write("### 📅 Forecast Table")
-    st.dataframe(forecast_df, use_container_width=True)
-
-    fig5 = px.line(
+       fig5 = px.line(
         forecast_df,
         x="Month",
         y="Predicted NO2",
@@ -353,3 +350,8 @@ with tab5:
         title=f"Forecasted NO₂ for {city}"
     )
     st.plotly_chart(fig5, use_container_width=True)
+
+    st.write("### 📅 Forecast Table")
+    st.dataframe(forecast_df, use_container_width=True)
+
+ 
